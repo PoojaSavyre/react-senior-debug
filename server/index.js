0feb34c187d7dadaf6ceaa47686a9dda19524f06
poojakumari prajapati <poojaprajapati2417@gmail.com>
2026-02-16 17:27:20 +0530

    server port made dynamic

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import apiRoutes from './apiRoutes.js';
 import { setupWebSocket } from './wsHandler.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
